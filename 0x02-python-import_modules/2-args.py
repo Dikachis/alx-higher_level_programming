81e7f7fd73138011935cf8c952dc2ae22ec390bd
Dikachis <dikachi015safety@gmail.com>
2022-06-03 04:07:25 +0100

    Added files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     s = "s" if n == 1 or n > 2 else ""
     p = ":" if n > 1 else "."
 
-    print("{:d} argument{}{}".format(n -1, s, p))
+    print("{:d} argument{}{}".format(n - 1, s, p))
 
-    for i in range (1, n):
+    for i in range(1, n):
         print("{:d}: {}".format(i, sys.argv[i]))
