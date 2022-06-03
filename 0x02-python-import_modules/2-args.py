3958d32dd634456ee0e5b779c8301eab249cca11
Dikachis <dikachi015safety@gmail.com>
2022-06-03 03:38:56 +0100

    Added 2-argv file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+from add_0 import add
+
+if __name__ == "__main__":
+
+    n = len(sys.argv)
+    s = "s" if n == 1 or n > 2 else ""
+    p = ":" if n > 1 else "."
+
+    print("{:d} argument{}{}".format(n -1, s, p))
+
+    for i in range (1, n):
+        print("{:d}: {}".format(i, sys.argv[i]))
