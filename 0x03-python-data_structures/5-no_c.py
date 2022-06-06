e634fc669f4abcee804d98fd8b2ed4c5a02d138c
Dikachis <dikachi015safety@gmail.com>
2022-06-06 22:24:39 +0100

    Added file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = my_string.translate({ord('c'): None, ord('C'): None})
-    return new_string
+    return (my_string.translate({ord('c'): None, ord('C'): None}))
