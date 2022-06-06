4dc58d80bd255d567bd8563810a6cafcaa5896a1
Dikachis <dikachi015safety@gmail.com>
2022-06-06 15:22:05 +0100

    Added a replaced file

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    for name in names:
+        new_list = idx.replace("element", "new_element")
+        return (new_list)
