308ac606f1a9364a5290588579ca3957290df1c3
Dikachis <dikachi015safety@gmail.com>
2022-06-07 03:32:13 +0100

    Added tuple file

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = ()
-    tuple_b = ()
-    # tuple_a
+
     if len(tuple_a) < 2:
         tuple_a = (tuple_a[0], 0)
     elif len(tuple_a) >= 2:
