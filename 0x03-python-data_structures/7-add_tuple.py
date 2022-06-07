bdd15f0807467f9495acae4cc665accc6d71f4bf
Dikachis <dikachi015safety@gmail.com>
2022-06-07 03:14:35 +0100

    Added tuple file

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,8 +2,15 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a = ()
     tuple_b = ()
-    if len(tuple_a) < 2 or len(tuple_b) < 2:
-        return (a, 0)
-    elif len(tuple_a) >= 2 or len(tuple_b) >= 2:
-        return (a, b)
-    return (tuple(map(lambda a, b: a + b, tuple_a, tuple_b)))
+    # tuple_a
+    if len(tuple_a) < 2:
+        tuple_a = (tuple_a[0], 0)
+    elif len(tuple_a) >= 2:
+        tuple_a = (tuple_a[0], tuple_a[1])
+        # tuple_b
+    if len(tuple_b) < 2:
+        tuple_b = (tuple_b[0], 0)
+    elif len(tuple_b) >= 2:
+        tuple_b = (tuple_b[0], tuple_b[1])
+
+    return (tuple(map(lambda [0], [1]: [0] + [1], tuple_a, tuple_b)))
