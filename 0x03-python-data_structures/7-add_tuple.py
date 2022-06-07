3d501a423cbfb86bdced7be5f02922e21e711c1b
Dikachis <dikachi015safety@gmail.com>
2022-06-07 03:49:33 +0100

    Added tuple file

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,5 +11,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) >= 2:
         tuple_b = (tuple_b[0], tuple_b[1])
 
-    result = tuple(map(lambda [0], [1]: [0] + [1], tuple_a, tuple_b))
+    result = tuple(map(sum, zip(tuple_a, tuple_b)))
     return result
