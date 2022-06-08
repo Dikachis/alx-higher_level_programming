e99cfbd85b7b1ff2bc7867bae0f5ab79d5287443
Dikachis <dikachi015safety@gmail.com>
2022-06-08 22:16:08 +0100

    Added square matrix func

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,3 +2,8 @@
 def square_matrix_simple(matrix=[]):
     new_matrx = ([[c**2 for c in row] for row in matrix])
     return(new_matrx)
+
+for row in [matrix]:
+    for c in [row]:
+        return(c,end = " ")
+    return()
