c1b64b4c14983294abfad28f71c9cb90e6fda784
Dikachis <dikachi015safety@gmail.com>
2022-06-08 22:05:17 +0100

    My first README.md for this projct

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-new_matrx = ([[i**2 for i in row] for row in matrix])
-return(new_matrx)
+    new_matrx = ([[c**2 for c in row] for row in matrix])
+    return(new_matrx)
