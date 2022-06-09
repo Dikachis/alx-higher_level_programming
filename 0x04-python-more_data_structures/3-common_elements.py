b5ef4df2d8181fa8cc1b9e383d9bfd98ebba4706
Dikachis <dikachi015safety@gmail.com>
2022-06-09 02:08:14 +0100

    Added common elements files

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,7 +2,4 @@
 def common_elements(set_1, set_2):
     a_set = set(set_1)
     b_set = set(set_2)
-    if (a_set & b_set):
-        return (a_set & b_set)
-    else:
-        return ("No common elements")
+    return (a_set & b_set)
