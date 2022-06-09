a67e2d0480dd078566098f6b4a516e46c74d2bcf
Dikachis <dikachi015safety@gmail.com>
2022-06-09 04:13:47 +0100

    Added simple del file

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    for i in a_dictionary:
+        if i == key:
+            del a_dictionary[key]
+            break
+    return a_dictionary
