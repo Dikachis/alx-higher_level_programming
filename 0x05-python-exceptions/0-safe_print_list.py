0c635579be1a9e5328597b4589aa84b01e301ac6
Dikachis <dikachi015safety@gmail.com>
2022-06-20 22:38:17 +0100

    Added safe_pri_file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     r_num = 0
-    for i in my_list:
+    for i in range(x):
         try:
             print(my_list[i], end="")
             r_num += 1
