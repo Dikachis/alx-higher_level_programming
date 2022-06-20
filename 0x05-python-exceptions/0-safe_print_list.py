bc634d0418d2371a51a84d0b061db666d1e07b36
Dikachis <dikachi015safety@gmail.com>
2022-06-20 22:32:03 +0100

    Added safe_pri_file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    r_num = 0
+    for i in my_list:
+        try:
+            print(my_list[i], end="")
+            r_num += 1
+        except IndexError:
+            break
+    print()
+    return r_num
