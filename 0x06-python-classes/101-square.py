313d5281b5eb81e1c4faa374d74e572a3bc2d2fe
Dikachis <dikachi015safety@gmail.com>
2022-06-22 00:40:09 +0100

    Added a file that defines a square by: (based on 6-square.py)

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ Declares a square class """
+
+    def __init__(self, size=0, position=(0, 0)) -> None:
+        """
+        Intializes the attributes
+
+        Args:
+            size: size of square
+            position:  position of square
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """ Gets the private attribute to be used in class """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """ Gets the private attribute to be used in class """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value     # tuple contains 2 positive integers
+
+    def area(self):
+        """ Computes area of a square """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ Prints in stdout the square with the character # """
+        output = list()
+        if self.__size == 0:
+            return "\n"
+        else:
+            integer = 0
+            pos1, pos2 = self.__position
+            for new_line in range(pos2):
+                output.append("\n")
+            while integer < self.__size:
+
+                j = 0
+                while j < pos1:
+                    output.append(" ")  # replace position with space
+                    j += 1
+
+                number = 0
+                while number < self.__size:
+                    output.append("#")
+                    number += 1
+                if integer + 1 != self.__size:
+                    output.append("\n")
+                integer += 1
+            return "".join(output)
