87ed56367f016a1fc2a04297a59e801d6fdfe08c
Dikachis <dikachi015safety@gmail.com>
2022-06-29 20:57:21 +0100

    Updated some files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 This is the "0-add_integer" module.
-The 0-add_integer module supplies one function, add_integer(a, b).
+The 0-add_integer module supplies one function,
+add_integer(a, b).
 """
 
 
