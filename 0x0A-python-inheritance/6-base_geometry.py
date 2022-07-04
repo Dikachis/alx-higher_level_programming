83f51f0c265dd36fb7c82c4f26f47e9e1a676d46
Dikachis <dikachi015safety@gmail.com>
2022-07-04 23:10:15 +0100

    Added 6-base_geometry.py file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is a module container of the function 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """A class with public attribute area"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
