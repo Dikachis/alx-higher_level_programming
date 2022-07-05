4a40c7437793d8913ade1ba6f788458c63157e71
Dikachis <dikachi015safety@gmail.com>
2022-07-05 18:43:39 +0100

    Added some files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """""reads a text file(UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as file:
         files = file.read()
-        print(file.read(), end="")
+        print(files, end="")
