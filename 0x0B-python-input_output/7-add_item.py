702f501b64d92a4ca952596325e1c5e2dd935c67
Dikachis <dikachi015safety@gmail.com>
2022-07-05 23:53:26 +0100

    Added 7-add_item.py file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,16 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+my_list = []
 
 try:
     my_list = load_from_json_file(filename)
 except Exception:
-    my_list = []
-for sys.args in sys.argv[1:]:
-    my_list.append(sys.args)
+
+arg_len = len(argv)
+
+if arg_len > 1:
+    for i in range(1, arg_len):
+        my_list.append(argv[i])
+
     save_to_json_file(my_list, filename)
