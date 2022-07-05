06409a9267ab975c232ace9221ce35c73f9871ad
Dikachis <dikachi015safety@gmail.com>
2022-07-06 00:17:06 +0100

    Added 7-add_item.py file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,13 +3,12 @@
 script to save and load
 """
 from sys import argv
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-my_list = []
 
+filename = 'add_item.json'
+my_list = []
 try:
     my_list = load_from_json_file(filename)
 except Exception:
