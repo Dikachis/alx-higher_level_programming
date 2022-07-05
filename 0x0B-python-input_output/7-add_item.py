f71d6edf7b72402903dc33f1ed78c98eda45a2d2
Dikachis <dikachi015safety@gmail.com>
2022-07-05 23:20:33 +0100

    Added 7-add_item.py file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script to save and load
+"""
+import sys
+
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+try:
+    my_list = load_json(filename)
+except Exception:
+    my_list = []
+for args in sys.argv[1:]:
+    my_list.append(args)
+    save_json(my_list, filename)
