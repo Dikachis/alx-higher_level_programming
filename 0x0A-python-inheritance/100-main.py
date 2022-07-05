e5aaf821b91e81efe67c6793d8d3a473d865be64
Dikachis <dikachi015safety@gmail.com>
2022-07-05 16:01:26 +0100

    Added 100-my_int.py file

--- 0x0A-python-inheritance/100-main.py
+++ 0x0A-python-inheritance/100-main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 MyInt = __import__('100-my_int').MyInt
 
 my_i = MyInt(3)
--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-class of 100-my_int.py module
+Contains definition of class MyInt
 """
 
 
 class MyInt(int):
-    """class with int object"""
+    """Definition of class MyInt that inherits from class int"""
 
-    def __ee__(self, other_obj):
-        """equal equal method"""
-        return super().__ee__(other_obj)
+    def __eq__(self, other):
+        """Overrides equals, inverting it"""
+        return int(self) != int(other)
 
-    def __ne__(self, other_obj):
-        """not equal method"""
-        return super().__ne__(other_obj)
+    def __ne__(self, other):
+        """Overrides not-equals, inverting it"""
+        return int(self) == int(other)
