d6e1c595f5a7a539eba3f20c12ca67e8f8f3d11b
Dikachis <dikachi015safety@gmail.com>
2022-07-05 23:40:03 +0100

    Added 7-add_item.py file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,6 +3,7 @@
 script to save and load
 """
 import sys
+import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
