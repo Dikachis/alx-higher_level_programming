dcbea93d305cac8c40f16d63dd2a22b43001690e
Dikachis <dikachi015safety@gmail.com>
2022-07-05 20:22:16 +0100

    Added 2-append_write.py file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""The read_file function container"""
+
+
+def append_write(filename="", text=""):
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as file:
+        lines = file.read()
+        for line in lines:
+            text.append(str(line))
+        return (text)
