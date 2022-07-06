2fb9e13a596c44d4b424f3099a59b79e6186cd37
Dikachis <dikachi015safety@gmail.com>
2022-07-06 01:35:18 +0100

    Added 10-student.py file

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
 """
-"Student class" cotainer
+9-student Module
 """
 
 
 class Student:
-    """Representation of a student"""
-    def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+    """
+    Student class
+    """
 
-        def to_json(self, attrs=None):
-            """returns a dictionary representation of a Student instance
-            with specified attributes"""
-            if attrs is None:
-                return self.__dict__
+def __init__(self, first_name, last_name, age) -> None:
+    """
+    initialization special method
+        Args:
+            first_name: first name of student
+            last_name: last name of student
+            age: age of student
+    """
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
 
-            new_dict = {}
-            for a in attrs:
-                try:
-                    new_dict[a] = self.__dict__[a]
-                except Exception:
-                    pass
-            return new_dict
+def to_json(self, attrs=None):
+    """
+    Retrieves dictionary representation of Student
+        Args:
+            attrs: initial list
+            Returns: a dictionary representation of a Student
+    """
+    if attrs is None or not all([type(attr) is str for attr in attrs]):
+        return self.__dict__.copy()
+
+    dic_repr = dict()
+    for key, value in self.__dict__.items():
+        if key in attrs:
+            dic_repr[key] = value
+
+    return dic_repr
