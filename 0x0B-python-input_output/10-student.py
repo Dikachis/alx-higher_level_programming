e3f58c069e1d5911e955a3650f6aba63f8636826
Dikachis <dikachi015safety@gmail.com>
2022-07-06 01:08:39 +0100

    Added 10-student.py file

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,7 +6,7 @@
 
 class Student:
     """Representation of a student"""
-    def __init__(self, first_name, last_name, age) ->None:
+    def __init__(self, first_name, last_name, age):
         """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
@@ -16,7 +16,7 @@ class Student:
         """returns a dictionary representation of a Student instance
         with specified attributes"""
     if attrs is None:
-        return self.__dict__.copy()
+        return self.__dict__
 
     new_dict = dict()
     for key, value in self.__dict__.items():
