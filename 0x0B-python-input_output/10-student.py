f57b34daf8d550a74094496ee487020039ffe7e7
Dikachis <dikachi015safety@gmail.com>
2022-07-06 01:02:32 +0100

    Added 10-student.py file

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+"Student class" cotainer
+"""
+
+
+class Student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of a Student instance
+        with specified attributes"""
+    if attrs is None:
+        return self.__dict__.copy()
+
+    new_dict = dict()
+    for key, value in self.__dict__.items():
+        if key in attrs:
+            new_dict[key] = value
+
+    return new_dict
