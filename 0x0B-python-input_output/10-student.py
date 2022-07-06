bab7f101459d93d390b3a7fdf5f58668acc8da3d
Dikachis <dikachi015safety@gmail.com>
2022-07-06 01:55:39 +0100

    Added some files

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,42 +1,37 @@
 #!/usr/bin/python3
 """
-9-student Module
+    Module for class Student
 """
 
 
 class Student:
     """
-    Student class
+        A class students that defines a student by:
+        Attributes:
+            first_name (str): name of student.
+            last_name (str): name of student.
+            age (int): age of student.
+        Methods:
+            __init__ - initializes the Student instance.
+            to_json - retrieves dictionary repr of Student instance.
     """
+    def __init__(self, first_name, last_name, age):
+        """
+            Initialises Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
+    def to_json(self, attr=None):
+        """
+            retrieves a dictionary representation of Student.
+            Args:
+                attr (list): attribute names that are to be retrieved.
+        """
 
-def __init__(self, first_name, last_name, age) -> None:
-    """
-    initialization special method
-        Args:
-            first_name: first name of student
-            last_name: last name of student
-            age: age of student
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-
-
-def to_json(self, attrs=None):
-    """
-    Retrieves dictionary representation of Student
-        Args:
-            attrs: initial list
-
-        Returns: a dictionary representation of a Student
-    """
-    if attrs is None or not all([type(attr) is str for attr in attrs]):
-        return self.__dict__.copy()
-
-    dic_repr = dict()
-    for key, value in self.__dict__.items():
-        if key in attrs:
-            dic_repr[key] = value
-
-    return dic_repr
+        if attr is not None:
+            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+            return res
+        else:
+            return self.__dict__
