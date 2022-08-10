05320f561d60484411aeeb683e486bb1d45afebc
Onyedikachi <dikachi015safety@gmail.com>
2022-08-09 20:04:48 -0700

    Added a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,11 +4,10 @@
 -- INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
 
 -- OR
-
 CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
- 	name VARCHAR(256),
-	score in
+	name VARCHAR(256),
+	score int
 );
 
 INSERT INTO second_table (id, name, score)
