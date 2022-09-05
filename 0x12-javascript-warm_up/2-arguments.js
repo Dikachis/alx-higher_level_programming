e69d1d8d1112cbfebd497b7b8d3298f781badc0d
Dikachis <dikachi015safety@gmail.com>
2022-09-05 23:29:07 +0100

    Added 2-arguments.js file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-
-const output = !process.argv[2] ? 'No argument' : process.argv[2];
-console.log(output);
+if (process.argv[2] === undefined) {
+    console.log('No argument');
+  } else {
+    console.log(process.argv[2]);
+  }
