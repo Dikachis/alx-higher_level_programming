cbc86cecb3ad7ea96aaa200d4665efdfd57b9db5
Dikachis <dikachi015safety@gmail.com>
2022-09-05 22:23:15 +0100

    Added 1-multi_languages.js file

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const my_string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const my_str of my_string) console.log(my_str);
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const lang of languages) console.log(lang);
