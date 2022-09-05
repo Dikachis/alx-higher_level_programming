99ab814693388056682eeae031b3fe7f1efe7d83
Dikachis <dikachi015safety@gmail.com>
2022-09-05 23:19:34 +0100

    Added 2-arguments.js file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
-'use strict';
-let arg = process.argv[2];
-if (arg === undefined) {
-  console.log('No argument');
-} else {
-  console.log(arg);
-}
+
+/**
+ * a script that prints a message depending
+ * on the number of arguments passed:
+ * if no arguments are passed to the script, print “No argument”
+ * if only one argument is passed to the script, print “Argument found”
+ * Otherwise, print “Arguments found”
+ */
+ if (process.argv[2]) {
+    console.log(process.argv[2]);
+  } else {
+    console.log('No argument');
+  }
