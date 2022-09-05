fc7024087a4ec7aa68c674878d0eb357f8289c00
Dikachis <dikachi015safety@gmail.com>
2022-09-05 22:54:12 +0100

    Added 1-multi_languages.js file

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-console.log(myVar.join('\n'));
+/*const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+console.log(myVar.join('\n'));*/
+
+'use strict';
+console.log('C is fun\nPython is cool\nJavascript is amazing');
