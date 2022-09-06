3461c91040d721d1b5d9d1ae09c87af2d26a75d5
Dikachis <dikachi015safety@gmail.com>
2022-09-06 01:30:19 +0100

    Fixed bugs in 2-arguments.js file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
-if (process.argv[2] === undefined) {
+if (process.argv.length === 2) {
     console.log('No argument');
-} else {
-    if (process.argv[2]) {
-        console.log('Argument found');
-    }
+} else if (process.argv.length === 3) {
+    console.log('Argument found');
+} else if (process.argv.length > 3) {
+    console.log('Arguments found');
 }
