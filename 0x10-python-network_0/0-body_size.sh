cc1af81228b02e90496af5897e02ab1d1b074b4f
Dikachis <dikachi015safety@gmail.com>
2022-09-26 23:56:46 +0000

    Added my README.md & 0-body_size.sh files

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+# The script should be found when search: 
+# https://stackoverflow.com/questions/4497759/how-to-get-remote-file-size-from-a-shell-script
+# but, we want to pass the URL as an argument in the command line, therefore we replace URL with $1;
+
 curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
