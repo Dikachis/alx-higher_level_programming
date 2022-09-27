98b4574282a73ca9896e803d0f9e1fa21e3b8987
Dikachis <dikachi015safety@gmail.com>
2022-09-27 00:20:42 +0000

    Added my README.md & 0-body_size.sh files

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -so /dev/null -w '%{size_download}\n' "$1"
+# curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
